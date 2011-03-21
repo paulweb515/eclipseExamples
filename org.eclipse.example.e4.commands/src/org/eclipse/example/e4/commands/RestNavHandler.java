ce22d2d4120111acd5214db550c2a8c8c420fc70
paulweb515 <paulweb515@gmail.com>
2011-03-21 08:42:14 -0700

    handler with canExecute

--- org.eclipse.example.e4.commands/src/org/eclipse/example/e4/commands/RestNavHandler.java
+++ org.eclipse.example.e4.commands/src/org/eclipse/example/e4/commands/RestNavHandler.java
@@ -11,14 +11,28 @@
 
 package org.eclipse.example.e4.commands;
 
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 /**
  * @since 1.0
  *
  */
 public class RestNavHandler {
+	@CanExecute
+	public boolean canExecute(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			return ! ((IStructuredSelection)selection).isEmpty();
+		}
+		return false;
+	}
 	@Execute
 	public void reset(final MPart part, final IResetable resetablePart) {
 		System.out.println("Resetting: " + part.getLabel());
